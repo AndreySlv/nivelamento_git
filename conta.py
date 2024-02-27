4aa00c80ac52dd73ae2e94a27079db1a829f7978
ANDREYSV <andreysilva2024@gmail.com>
2024-02-27 16:47:42 -0300

    saque

--- conta.py
+++ conta.py
@@ -32,15 +32,18 @@ while True:
 
     elif opcao == '2':
         valor_saque = float(input("Digite o valor do saque: "))
-        extratos.append({
+
+        if valor_saque > saldo:
+            print("Saldo indisponível")
+            extratos.append({
                 "tipo": "Saque",
                 "valor": valor_saque,
                 "horario": datetime.now().strftime('%H:%M:%S'),
                 "data": datetime.now().strftime('%Y-%m-%d')
             })
-        # -----------------------------------------
-        # Desenvolva a lógica da validação do saque
-        # -----------------------------------------
+        else:
+            saldo = saldo - valor_saque
+            print(f"Saque de {valor_saque} efetuado com sucesso!")
 
     elif opcao == '3':
         print(f'\nExtrato bancário:')
